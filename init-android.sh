b41f5071fe87a9573048f571dd54548be11482a4
biezhihua <biezhihua@gmail.com>
2019-01-03 20:33:06 +0800

    shell: ffmpeg repository

--- init-android.sh
+++ init-android.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env bash
 
+RED='\033[0;31m'
+Green='\033[0;33m'
+NC='\033[0m' # No Color
+
 FFMPEG_UPSTREAM=https://github.com/FFmpeg/FFmpeg.git
-FFMPEG_BRANCH=origin/release/4.1
-FFMPEG_LOCAL_REPO=repository/ffmpeg
+FFMPEG_VERSION=4.1
+FFMPEG_BRANCH=origin/release/$FFMPEG_VERSION
+FFMPEG_LOCAL_REPO=repository/ffmpeg-$FFMPEG_VERSION
 
 # http://www.runoob.com/linux/linux-comm-set.html
 # set指令能设置所使用shell的执行方式，可依照不同的需求来做设置
@@ -12,20 +17,30 @@ set -e
 
 git --version
 
-echo "== pull ffmpeg ($FFMPEG_UPSTREAM) base repository $FFMPEG_BRANCH =="
+echo "--------------------"
+echo -e "${RED}[*] pull ffmpeg ($FFMPEG_UPSTREAM) base branch $FFMPEG_BRANCH ${NC}"
+echo "--------------------"
+
+# if [ -d repository/ffmpeg-${FFMPEG_VERSION} ]; then
+#     rm -rf ./repository/ffmpeg-$FFMPEG_VERSION
+# fi 
 
 sh tools/pull-repo-base.sh $FFMPEG_UPSTREAM $FFMPEG_LOCAL_REPO
 
 function pull_fork()
 {
-    echo "== pull ffmpeg fork $1 =="
+    echo ""
+    echo "--------------------"
+    echo -e "${RED}[*] pull ffmpeg fork ffmpeg-$1 ${NC}"
+    echo "--------------------"
+    
     if [[ -d android/contrib/ffmpeg-$1 ]]; then
         rm -rf android/contrib/ffmpeg-$1
     fi
 
     sh tools/pull-repo-ref.sh $FFMPEG_UPSTREAM android/contrib/ffmpeg-$1 ${FFMPEG_LOCAL_REPO}
     cd android/contrib/ffmpeg-$1
-    git checkout -b splayer ${FFMPEG_BRANCH}
+    git checkout -b build_tools ${FFMPEG_BRANCH}
     cd -
 }
 
@@ -34,4 +49,6 @@ pull_fork "armv8a"
 pull_fork "x86"
 pull_fork "x86_64"
 
-./init-config.sh
+echo "--------------------"
+echo -e "${RED}[*] Finish pull ffmpeg ${NC}"
+echo "--------------------"
