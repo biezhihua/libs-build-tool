bc69197d7b7095f81b206cd6340f157d3843159b
biezhihua <biezhihua@gmail.com>
2019-02-11 19:23:27 +0800

    shell: Fix Release Diable Debug

--- android/do-compile-android-ffmpeg.sh
+++ android/do-compile-android-ffmpeg.sh
@@ -313,7 +313,7 @@ case "$FF_BUILD_OPT" in
     ;;
     *)
         FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-optimizations"
-        FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-debug"
+        FF_CFG_FLAGS="$FF_CFG_FLAGS --disable-debug"
         FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-small"
     ;;
 esac
